d5082f69eed9ea8b6497908190bcd03fc70379b3
JaninaAlona <jelona@posteo.net>
2022-08-17 11:30:00 +0200

    source cmd added

--- github_push_pull/github_push_pull.sh
+++ github_push_pull/github_push_pull.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
-action="pull"
+ACTION="pull"
 export PW=${PW:-n}
 
 function login() {
     if [ "$PW" == "y" ] || [ "$PW" == "Y" ];then
-        if [ -f .env ]; then
+        if [ -f source .env ]; then
             export $(cat .env | xargs)
             if grep -Fq GITHUB_USER .env && grep -Fq GITHUB_TOKEN .env
             then
                 if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_TOKEN" ];then
                     echo "environment variables unset in .env file"
                 else
-                    ./ghloginbot.exp $action $GITHUB_USER $GITHUB_TOKEN
+                    source $(dirname "$0")/ghloginbot.exp $ACTION $GITHUB_USER $GITHUB_TOKEN
                 fi
             else
                 echo "environment variables missing in .env file"
@@ -21,9 +21,9 @@ function login() {
             echo ".env file missing"
         fi
     elif [ "$PW" == "n" ] || [ "$PW" == "N" ];then
-        if [ "$action" == "push" ];then
+        if [ "$ACTION" == "push" ];then
             git push
-        elif [ "$action" == "pull" ];then
+        elif [ "$ACTION" == "pull" ];then
             git pull
         fi
     fi
@@ -34,7 +34,7 @@ then
     if [ "$1" == "pull" ];then
         login
     elif [ "$1" == "push" ];then 
-        action="push"
+        ACTION="push"
         git add * && \
         git commit -m "$2" && \
         login
